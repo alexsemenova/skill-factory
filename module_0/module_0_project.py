d1a2b55569c5e5848b8dd8321f695989e7c817ed
alexs15 <alexandra.semenova96@gmail.com>
2021-05-10 23:08:39 +0200

    add

--- module_0/module_0_project.py
+++ module_0/module_0_project.py
@@ -20,3 +20,16 @@ number = np.random.randint(1, 101)
 print("Загадано число от 1 до 100")
 print('Загаданное число', number)
 print(f'Число было угадано за {guess(number)} попыток')
+
+def score_game(game_core):
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(guess())
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+score_game(guess)
+
